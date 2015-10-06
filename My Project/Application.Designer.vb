247fefaa2277640863ce9221671d84b7c688e8c1
DeepDolphin <davidjyee@gmail.com>
2015-10-06 09:54:15 -0700

    rename

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.XGame.MainForm
+            Me.MainForm = Global.JackAndTheBox.MainForm
         End Sub
     End Class
 End Namespace
