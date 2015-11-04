8aabebe17a5e5f9b73062de31b1b3b45534b9baf
DeepDolphin <davidjyee@gmail.com>
2015-11-04 14:39:56 -0800

    added crate stuff

--- Crate.vb
+++ Crate.vb
@@ -1,9 +1,28 @@
 ﻿Public Class Crate
     Inherits GameObject
 
-    Public Sub New(Room As Room, Position As Vector3, Health As Integer)
-        MyBase.New(My.Resources.Crate, Room, Position, Health)
+    Public Sub New(Room As Room, Position As Vector3)
+        MyBase.New(My.Resources.Crate, Room, Position, 10)
     End Sub
 
+    Public Sub New(Sprite As Sprite, Room As Room, Position As Vector3)
+        MyBase.New(My.Resources.Crate, Room, Position, 10)
+    End Sub
 
+End Class
+
+Public Class ExplosiveCrate
+    Inherits Crate
+
+    Public Sub New(Room As Room, Position As Vector3)
+        MyBase.New(Room, Position)
+    End Sub
+
+    Public Sub Explode()
+        Dim objects As List(Of GameObject) = getNearList(10)
+
+
+
+
+    End Sub
 End Class
